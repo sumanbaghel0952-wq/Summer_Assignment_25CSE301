a88c7859fa81ba5af2509ee28659a46d49c4d053
sumanbaghel0952-wq <sumanbaghel0952@gmail.com>
2026-06-10 15:59:45 +0530

    Added Day1 programs

---
+++ Product_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, rem, product = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        rem = n % 10;
+        product *= rem;
+        n = n / 10;
+    }
+
+    printf("Product of digits = %d", product);
+
+    return 0;
+}

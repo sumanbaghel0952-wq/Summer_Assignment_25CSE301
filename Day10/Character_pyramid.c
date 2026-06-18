7effb0753154ab052e6ab28a685939fdf32c46fd
sumanbaghel0952-wq <sumanbaghel0952@gmail.com>
2026-06-18 21:08:43 +0530

    Add Day 10 C programs

---
+++ Day10/Character_pyramid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 1; i <= 5; i++) {
+
+        for(j = 0; j < i; j++) {
+            printf("%c", 'A' + j);
+        }
+
+        for(j = i - 2; j >= 0; j--) {
+            printf("%c", 'A' + j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}

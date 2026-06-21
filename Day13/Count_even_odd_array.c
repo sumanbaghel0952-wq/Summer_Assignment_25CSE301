7569f40be16286f7dc8a729776d3af25a164dfd2
sumanbaghel0952-wq <sumanbaghel0952@gmail.com>
2026-06-21 14:16:21 +0530

    Add Day13 programs

---
+++ Day13/Count_even_odd_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even elements = %d\n", even);
+    printf("Odd elements = %d", odd);
+
+    return 0;
+}
